68b679d678741c3748d4e255f07f6f05f3e26d8f
syu1 <samyu1029@outlook.com>
2018-02-25 13:09:28 -0500

    ok
    ok

--- Just-Java-master/app/src/main/java/com/example/android/justjava/FoodItem.java
+++ Just-Java-master/app/src/main/java/com/example/android/justjava/FoodItem.java
@@ -1,34 +1,42 @@
 package com.example.android.justjava;
 
-/**
- * Created by samyu on 2/25/2018.
- */
-
-
-
 public class FoodItem {
 
     private String name;
+    private String restaurant;
     private double cost;
     private int proteinAmount;
     private int carbAmount;
     private int calorieAmount;
     private int sodiumAmount;
     private int totalFat;
+    private double proteinPercent;
+    private double carbPercent;
+    private double calPercent;
+    private double compositeScore;
 
-    public FoodItem(String name, double cost, int protein, int carb, int calorie, int sodium, int fat)
+    public FoodItem(String name, String restaurant, double cost, int protein, int carb, int calorie, int sodium, int fat, double proteinPercent, double carbPercent, double calPercent, double compositeScore)
     {
         this.name = name;
+        this.restaurant = restaurant;
         this.cost = cost;
-        proteinAmount = protein;
-        carbAmount = carb;
-        calorieAmount = calorie;
-        sodiumAmount = sodium;
-        totalFat = fat;
+        this.proteinAmount = protein;
+        this.carbAmount = carb;
+        this.calorieAmount = calorie;
+        this.sodiumAmount = sodium;
+        this.totalFat = fat;
+        this.proteinPercent = proteinPercent;
+        this.carbPercent = carbPercent;
+        this.calPercent = calPercent;
+        this.compositeScore = compositeScore;
     }
 
     public String getName(){
         return name;
+    }
+
+    public String getRestaurant(){
+        return restaurant;
     }
 
     public double getCost(){
@@ -53,6 +61,44 @@ public class FoodItem {
 
     public int getFat(){
         return totalFat;
+    }
+
+    public void setProteinPercent(double p){
+        proteinPercent = p;
+    }
+
+    public double getProteinPercent(){
+        return proteinPercent;
+    }
+
+    public void setCarbPercent(double p){
+        carbPercent = p;
+    }
+
+    public double getCarbPercent(){
+        return carbPercent;
+    }
+
+    public void setCalPercent(double p){
+        calPercent = p;
+    }
+
+    public double getCalPercent(){
+        return calPercent;
+    }
+
+    public void setCompScore(double score){
+        compositeScore = score;
+    }
+
+    public double getCompScore(){
+        return compositeScore;
+    }
+
+    public String toString(){
+
+        return String.format("Food Name: %s/nCalories: %d/nProtein: %d/nCarbs: %d/nFat: %d/nFrom Restaurant: %s/n", name, calorieAmount, proteinAmount,
+                carbAmount, totalFat, restaurant);
     }
 
 }
--- Just-Java-master/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ Just-Java-master/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -27,7 +27,9 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
+import java.lang.reflect.Array;
 import java.text.NumberFormat;
+import java.util.ArrayList;
 
 /**
  * This app displays an order form to order coffee.
@@ -47,58 +49,154 @@ public class MainActivity extends AppCompatActivity {
         EditText nameField = (EditText) findViewById(R.id.name_field);
         Editable nameEditable = nameField.getText();
 
+        FoodDatabase foodBase= new FoodDatabase();
+        ArrayList<FoodItem> foodList=foodBase.createBase();
+        Optimization optimize = new Optimization();
+
         String price = nameEditable.toString();
 
         double calcPrice = Double.parseDouble(price);
+
+        int calories;
+        int sodium;
         int protein;
         int carb;
         int fat;
 
+
+        RadioButton caloriesHigh = (RadioButton) findViewById(R.id.calories_high_field);
+        boolean hasHighColories = caloriesHigh.isChecked();
+
+
+        RadioButton caloriesLow = (RadioButton) findViewById(R.id.calories_low_field);
+        boolean hasLowColories = caloriesLow.isChecked();
+
+        if (hasHighColories == true)
+        {
+            calories = 2;
+        }
+        else if(hasLowColories == true)
+        {
+            calories = 1;
+        }
+        else
+        {
+            calories = 0;
+        }
+
         // Figure out if the user wants whipped cream topping
+        RadioButton carbsHigh = (RadioButton) findViewById(R.id.carbs_high_field);
+        boolean hasHighCarbs = carbsHigh.isChecked();
+
+
+        RadioButton carbsLow = (RadioButton) findViewById(R.id.carbs_low_field);
+        boolean hasLowCarbs = carbsLow.isChecked();
+
+        if (hasHighCarbs == true)
+        {
+            carb = 2;
+        }
+        else if(hasLowCarbs == true)
+        {
+            carb = 1;
+        }
+        else
+        {
+            carb = 0;
+        }
+
+
+        RadioButton fatHigh = (RadioButton) findViewById(R.id.fat_high_field);
+        boolean hasHighFat = fatHigh.isChecked();
+
+
+        RadioButton fatLow = (RadioButton) findViewById(R.id.fat_low_field);
+        boolean hasLowFat = fatLow.isChecked();
+
+        if (hasHighFat == true)
+        {
+            fat = 2;
+        }
+        else if(hasLowFat == true)
+        {
+            fat = 1;
+        }
+        else
+        {
+            fat = 0;
+        }
+
         RadioButton proteinHigh = (RadioButton) findViewById(R.id.protein_high_field);
         boolean hasHighProtein = proteinHigh.isChecked();
 
         RadioButton proteinLow = (RadioButton) findViewById(R.id.protein_low_field);
         boolean hasLowProtein = proteinLow.isChecked();
 
-        // Figure out if the user wants choclate topping
-        RadioButton carbsHigh = (RadioButton) findViewById(R.id.carbs_high_field);
-        boolean hasHighCarbs = carbsHigh.isChecked();
+        if (hasHighProtein == true)
+        {
+            protein = 2;
+        }
+        else if(hasLowProtein == true)
+        {
+            protein =1;
+        }
+        else
+        {
+            protein = 0;
+        }
+        RadioButton sodiumHigh = (RadioButton) findViewById(R.id.sodium_high_field);
+        boolean hasHighSodium = sodiumHigh.isChecked();
 
-        RadioButton carbsLow = (RadioButton) findViewById(R.id.carbs_low_field);
-        boolean hasLowCarbs = carbsLow.isChecked();
+        RadioButton sodiumLow = (RadioButton) findViewById(R.id.sodium_low_field);
+        boolean hasLowSodium = sodiumLow.isChecked();
 
-        RadioButton fatHigh = (RadioButton) findViewById(R.id.fat_high_field);
-        boolean hasHighFat = fatHigh.isChecked();
+        if (hasHighSodium == true)
+        {
+            sodium = 2;
+        }
+        else if(hasLowProtein == true)
+        {
+            sodium =1;
+        }
+        else
+        {
+            sodium = 0;
+        }
+        ArrayList<FoodItem> suggestedFoods = optimize.suggest(foodList,calcPrice,protein,carb,calories);
+        FoodItem top1 = suggestedFoods.get(0);
+        FoodItem top2 = suggestedFoods.get(1);
+        FoodItem top3 = suggestedFoods.get(2);
 
-        RadioButton fatLow = (RadioButton) findViewById(R.id.fat_low_field);
-        boolean hasLowFat = fatLow.isChecked();
-        FoodItem food1 = new FoodItem("Burger",calcPrice,20,10,400,15,17) ;
-        FoodItem food2 = new FoodItem("Burrito",calcPrice,17,30,500,30,21) ;
-        FoodItem food3 = new FoodItem("Noodles",calcPrice,22,42,350,45,8) ;
+
+
         String message = "";
 
-        message += " Your optimal foods for $"+food1.getCost();
-        message += "\n Food Name: " +food1.getName();
-        message += "\n Calories: " +food1.getCalories() +"g";
-        message += "\n Protein: " +food1.getProtein() +"g";
-        message += "\n Carb: " +food1.getCarbs() +"g";
-        message += "\n Fat: " + food1.getFat()+"g";
-        message += "\n From: Five Guys\n";
+        message += " Your optimal lowest priced foods";
+        message += "\n base on DV for under: $"+calcPrice +"\n";
 
-        message += "\n Food Name: " +food2.getName();
-        message += "\n Calories: " +food2.getCalories() +"g";
-        message += "\n Protein: " +food2.getProtein() +"g";
-        message += "\n Carb: " +food2.getCarbs() +"g";
-        message += "\n Fat: " + food2.getFat()+"g";
-        message += "\n From: Chipotle\n";
+        message += "\n Price: " +top1.getCost();
+        message += "\n Food Name: " +top1.getName();
+        message += "\n Calories: " +top1.getCalories() +"g";
+        message += "\n Protein: " +top1.getProtein() +"g";
+        message += "\n Carb: " +top1.getCarbs() +"g";
+        message += "\n Fat: " + top1.getFat()+"g";
+        message += "\n From: "+ top1.getRestaurant()+ "\n";
 
-        message += "\n Food Name: " +food3.getName();
-        message += "\n Calories: " +food3.getCalories() +"g";
-        message += "\n Protein: " +food3.getProtein() +"g";
-        message += "\n Carb: " +food3.getCarbs() +"g";
-        message += "\n Fat: " + food3.getFat()+"g";
-        message += "\n From: Sichuan Gourmet";
+        message += "\n Price: " +top2.getCost();
+        message += "\n Food Name: " +top2.getName();
+        message += "\n Calories: " +top2.getCalories() +"g";
+        message += "\n Protein: " +top2.getProtein() +"g";
+        message += "\n Carb: " +top2.getCarbs() +"g";
+        message += "\n Fat: " + top2.getFat()+"g";
+        message += "\n From: "+ top2.getRestaurant()+ "\n";
+
+        message += "\n Price: " +top3.getCost();
+        message += "\n Food Name: " +top3.getName();
+        message += "\n Calories: " +top3.getCalories() +"g";
+        message += "\n Protein: " +top3.getProtein() +"g";
+        message += "\n Carb: " +top3.getCarbs() +"g";
+        message += "\n Fat: " + top3.getFat()+"g";
+        message += "\n From: "+ top3.getRestaurant();
 
         displayFood(message);
 
