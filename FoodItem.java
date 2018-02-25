6326c2c71312e5f76835095dbd823c1eaf24b5df
Mufaro Nyereyemhuka <mbn8@pitt.edu>
2018-02-25 11:20:25 -0500

    Update FoodItem.java

--- FoodItem.java
+++ FoodItem.java
@@ -1,29 +1,40 @@
-//package com.example.android.justjava;
-
-public class FoodItem implements Comparable<FoodItem>{
+public class FoodItem {
 	
 	private String name;
-	private String restaurant = "Generic Restaurant";
+	private String restaurant;
 	private double cost;
 	private int proteinAmount;
 	private int carbAmount;
 	private int calorieAmount;
 	private int sodiumAmount;
 	private int totalFat;
+	private double proteinPercent;
+	private double carbPercent;
+	private double calPercent;
+	private double compositeScore;
 	
-	public FoodItem(String name, double cost, int protein, int carb, int calorie, int sodium, int fat)
+	public FoodItem(String name, String restaurant, double cost, int protein, int carb, int calorie, int sodium, int fat, double proteinPercent, double carbPercent, double calPercent, double compositeScore)
 	{
 		this.name = name;
+		this.restaurant = restaurant;
 		this.cost = cost;
 		this.proteinAmount = protein;
 		this.carbAmount = carb;
 		this.calorieAmount = calorie;
 		this.sodiumAmount = sodium;
 		this.totalFat = fat;
+		this.proteinPercent = proteinPercent;
+		this.carbPercent = carbPercent;
+		this.calPercent = calPercent;
+		this.compositeScore = compositeScore;
 	}
 	
 	public String getName(){
 		return name;
+	}
+	
+	public String getRestaurant(){
+		return restaurant;
 	}
 	
 	public double getCost(){
@@ -49,15 +60,41 @@ public class FoodItem implements Comparable<FoodItem>{
 	public int getFat(){
 		return totalFat;
 	}
-
-	@Override 
-	public int compareTo(FoodItem other){
-		return this.name.compareTo(other.getName());
 	
+	public void setProteinPercent(double p){
+		proteinPercent = p;
+	}
+	
+	public double getProteinPercent(){
+		return proteinPercent;
+	}
+	
+	public void setCarbPercent(double p){
+		carbPercent = p;
+	}
+	
+	public double getCarbPercent(){
+		return carbPercent;
+	}
+	
+	public void setCalPercent(double p){
+		calPercent = p;
+	}
+	
+	public double getCalPercent(){
+		return calPercent;
+	}
+	
+	public void setCompScore(double score){
+		compositeScore = score;
+	}
+	
+	public double getCompScore(){
+		return compositeScore;
 	}
 
-
 	public String toString(){
+
 		return String.format("Food Name: %s/nCalories: %d/nProtein: %d/nCarbs: %d/nFat: %d/nFrom Restaurant: %s/n", name, calorieAmount, proteinAmount, 
 		carbAmount, totalFat, restaurant);
 	}
