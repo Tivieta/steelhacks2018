45b7470af69c810b5a98dfef5c5a8a5fc8482fde
Mufaro Nyereyemhuka <mbn8@pitt.edu>
2018-02-25 03:52:45 -0500

    Update FoodDatabase.java

--- FoodDatabase.java
+++ FoodDatabase.java
@@ -12,6 +12,15 @@ public class FoodDatabase {
 		database.add(new FoodItem("Medium French Fries", "McDonald's", 1.79, 4, 44, 510, 190, 16));
 		database.add(new FoodItem("Ranch Snack Wrap (Crispy)", "McDonald's", 1.69, 15, 32, 360, 810, 20));
 		
+		database.add(new FoodItem("Black Forest Ham (6 inch)", "Subway", 3.75, 18, 46, 290, 800, 5));
+		database.add(new FoodItem("Black Forest Ham (Footlong)", "Subway", 5.50, 36, 92, 580, 1600, 10));
+		database.add(new FoodItem("Meatball Marinara (6 inch)", "Subway", 3.75, 20, 53, 460, 1090, 18));
+		database.add(new FoodItem("Meatball Marinara (Footlong)", "Subway", 5.50, 40, 106, 920, 2180, 36));
+		database.add(new FoodItem("Italian B.M.T. (6 inch)", "Subway", 4.25, 19, 43, 390, 1330, 17));
+		database.add(new FoodItem("Italian B.M.T. (Footlong)", "Subway", 6.75, 38, 86, 780, 2660, 34));
+		
+		
+		
 		return database;
 		
 	}
