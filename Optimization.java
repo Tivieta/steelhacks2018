2e553a3b75785e320980e67c991f16e523670c11
Mufaro Nyereyemhuka <mbn8@pitt.edu>
2018-02-25 11:18:11 -0500

    Update Optimization.java

--- Optimization.java
+++ Optimization.java
@@ -1,75 +1,201 @@
-import java.util.Scanner;
-import java.util.ArrayList;
+import java.util.*;
 
-public class Optimization {
+public class Optimization{
+	private final static int suggestedProtein = 50;	// suggested daily protein intakes
+	private final static int suggestedCarbs = 300;	// suggested daily carbohydrate intakes
+	private final static int suggestedCalories = 2000;	// suggested daily calorie intakes
+	private final static int suggestedSodium = 2400;	// suggested daily sodium intakes
+	private final static int suggestedFat = 65;	// suggested daily fat intakes
+	
 	public static void main(String[] args){
-		//Order: cost, protein, carb, calorie, sodium (in mg), fat
-		FoodItem burger = new FoodItem("Bob's Burger", 6.5, 26, 0, 271, 440, 18);
-		FoodItem crushOrange = new FoodItem("Crush Orange", 1.0, 0, 43, 160, 70, 0);
-		FoodItem cookies = new FoodItem("Grandma's Cookies", 3.0, 0, 30, 30, 10, 15);
-		FoodItem iceCream = new FoodItem("Ice Cream", 3.50, 2, 13, 120, 45, 7);
 		
-		ArrayList<FoodItem> foodItems = new ArrayList<FoodItem>();
-		foodItems.add(burger);
-		foodItems.add(crushOrange);
-		foodItems.add(cookies);
-		foodItems.add(iceCream);
+		FoodDatabase database = new FoodDatabase();
+		List<FoodItem> foodDataBase = database.createBase();
+		
+		List<FoodItem> foodItems = new ArrayList<FoodItem>(foodDataBase); // copy of ArrayList to alter
 		
 		Scanner scanner = new Scanner(System.in);
 		
 		System.out.print("Cost maximum:  ");
 		double costMax = scanner.nextDouble();
+		ArrayList<Integer> removeIndices = new ArrayList<Integer>();
+		
+		
+		try{
+			
+		}catch(IndexOutOfBoundsException e){
+			
+		}
+		
+		// Remove items with prices greater than the maximum cost provided by the user
+		for(int i = 0; i < foodDataBase.size(); i++ ){
+			if(foodDataBase.get(i).getCost() > costMax){
+				foodItems.remove(i);
+				i--;
+				System.out.println("i: " + i);
+			}
+		}
+		
+		/*for(int i = 0; i < removeIndices.size(); i++){
+			foodItems.remove((int) removeIndices.get(i));
+		}*/
 		
 		System.out.print("\nProtein - High (2), Low (1), n/a (0): ");
 		int proteinChoice = scanner.nextInt();
 		
-		System.out.print("\nCarbs - High (2), Low (1), n/a (0): ");
+		System.out.print("Carbs - High (2), Low (1), n/a (0): ");
 		int carbChoice = scanner.nextInt();
 		
-		System.out.print("\nCalorie - High (2), Low (1), n/a (0): ");
+		System.out.print("Calorie - High (2), Low (1), n/a (0): ");
 		int calorieChoice = scanner.nextInt();
 		
-		NormalizedSort protein = new NormalizedSort(foodItems,"high");
-
-		//FoodItem optimalChoice = optimize(foodItems, costMax, proteinChoice, carbChoice, calorieChoice);
+		System.out.println();
 		
-
-		System.out.println("The best food option is: " + optimalChoice.getName());
+		List<FoodItem> returnArray = suggest(foodItems, costMax, proteinChoice, carbChoice, calorieChoice);
+		
+		System.out.println("Top 3 Suggestions: ");
+		int j = 0;
+		
+		for(FoodItem item : returnArray){
+			
+			if(j == 3)
+				break;
+			
+			
+			
+			System.out.println("Name: " + item.getName());
+			System.out.println("Price: " + item.getCost());
+			System.out.println("Protein Percent: " + item.getProteinPercent());
+			System.out.println("Carb Percent: " + item.getCarbPercent());
+			System.out.println("Calorie Percent: " + item.getCalPercent());
+	
+			double compositeScore = item.getCompScore();
+			System.out.println("Composite Score: " + compositeScore);
+			
+			System.out.println();
+			j++;
+		}
 		
 	}
 	
-	public static FoodItem optimize(ArrayList<FoodItem> list, double costMax, int proteinChoice, int carbChoice, int calorieChoice){
-		FoodItem optimal = null;
+	public static ArrayList<FoodItem> suggest(List<FoodItem> foodItems, double costMax, int proteinChoice, int carbChoice, int calorieChoice){
+		List<FoodItem> returnArray = new ArrayList<FoodItem>();
 		
-		ArrayList<Integer> proteinList = new ArrayList<Integer>();
-		ArrayList<Integer> carbList = new ArrayList<Integer>();
-		ArrayList<Integer> calorieList = new ArrayList<Integer>();
-		
-		for(FoodItem item : list){
-			if(item.getCost() > costMax)
-				continue;
+		// Set percentages for the composite score
+		for(FoodItem food : foodItems){
+			double proteinPercent = (double) food.getProtein() / suggestedProtein;
+			food.setProteinPercent(proteinPercent);
 			
-			if(proteinChoice > 0){
-				proteinList.add(item.getProtein());
-			}
+			double carbPercent = (double) food.getCarbs() / suggestedCarbs;
+			food.setCarbPercent(carbPercent);
 			
-			if(carbChoice > 0){
-				carbList.add(item.getCarbs());
-			}
-			 
-			if(calorieChoice > 0){
-				calorieList.add(item.getCalories());
-			}
+			double caloriePercent = (double) food.getCalories() / suggestedCalories;
+			food.setCalPercent(caloriePercent);
+	
+		}
+		
+		
+		// If the user wants all 3 to be HIGH
+		if(proteinChoice == 2 && carbChoice == 2 && calorieChoice == 2){
 			
-		} // end for each loop
-		
-		return optimal;
-	}
-	
-	public static ArrayList<Integer> sort(ArrayList<Integer> list, int choice){
-		
-	}
-	
-	
+			Collections.sort(foodItems, new Comparator<FoodItem>() {
+		        @Override public int compare(FoodItem f1, FoodItem f2) {
+		        	
+		        	double percent2 = ( f2.getProteinPercent() + f2.getCarbPercent() + f2.getCalPercent() );
+		        	double percent1 = ( f1.getProteinPercent() + f1.getCarbPercent() + f1.getCalPercent() );
+		        	
+		        	f2.setCompScore(percent2);
+		        	f1.setCompScore(percent1);
+		        	
+		            return Double.compare( percent2, percent1 ); // Descending
+		        }
 
+		    });	
+			
+			returnArray = new ArrayList<FoodItem>(foodItems);
+			
+		} // end if(proteinChoice == 2 && carbChoice == 2 && calorieChoice == 2)
+		
+		// If the user wants all 3 to be LOW
+		else if(proteinChoice == 1 && carbChoice == 1 && calorieChoice == 1){
+					
+			Collections.sort(foodItems, new Comparator<FoodItem>() {
+				@Override public int compare(FoodItem f1, FoodItem f2) {
+				        	
+					double percent1 = ( f1.getProteinPercent() + f1.getCarbPercent() + f1.getCalPercent() );
+					double percent2 = ( f2.getProteinPercent() + f2.getCarbPercent() + f2.getCalPercent() );
+					
+					f1.setCompScore(percent1);
+					f2.setCompScore(percent2);
+				        	
+					return Double.compare( percent1, percent2 ); // Ascending
+				}
+
+			});	
+					
+			returnArray = new ArrayList<FoodItem>(foodItems);
+					
+		} // end if(proteinChoice == 2 && carbChoice == 2 && calorieChoice == 2)
+		
+		else{
+			
+			for(FoodItem item : foodItems){
+				ArrayList<Double> highOptions = new ArrayList<Double>();
+				ArrayList<Double> lowOptions = new ArrayList<Double>();
+				ArrayList<Double> noOptions = new ArrayList<Double>();
+				double highSum = 0.0;
+				double lowSum = 0.0;
+				
+				switchOptions(proteinChoice, item.getProteinPercent(), highOptions, lowOptions, noOptions);
+				switchOptions(carbChoice, item.getCarbPercent(), highOptions, lowOptions, noOptions);
+				switchOptions(calorieChoice, item.getCalPercent(), highOptions, lowOptions, noOptions);
+				
+				for(Double option : highOptions){
+					highSum += option;
+				}
+				
+				for(Double option : lowOptions){
+					lowSum += option;
+				}
+				
+				item.setCompScore(highSum - lowSum);
+				
+						
+			} // end for(FoodItem item : foodItems)
+			
+			Collections.sort(foodItems, new Comparator<FoodItem>() {
+				@Override public int compare(FoodItem f1, FoodItem f2) {
+				        	
+					double percent2 = ( f2.getCompScore() );
+					double percent1 = ( f1.getCompScore() );
+				        	
+					return Double.compare( percent2, percent1 ); // Descending
+				}
+
+			});	
+					
+			returnArray = new ArrayList<FoodItem>(foodItems);
+			
+		
+		} // end else
+		
+		return (ArrayList<FoodItem>) returnArray;
+	} // end suggest()
+	
+	public static void switchOptions(int option, double percent, ArrayList<Double> high, ArrayList<Double> low, ArrayList<Double> none){
+		
+		switch(option){
+			case 0:
+				none.add(percent);
+				break;
+			case 1:
+				low.add(percent);
+				break;
+			case 2:
+				high.add(percent);
+				break;
+		}
+		
+	}
+	
 }
